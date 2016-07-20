9e2e8c234f2fc1b640a1cb8817b2b9b6722d3c4b
Karen Ceron <1713110241@utec-tgo.edu.mx>
2016-07-19 22:13:06 -0500

    first commit

--- api/services/bot.js
+++ api/services/bot.js
@@ -1,5 +1,5 @@
-var TelegramBot=require('node-telegram-bot-api');
-var token='234117238:AAG4ZwnowxQQY6cU0OZX4YR_qu-ibJzJuxg';
+﻿var TelegramBot=require('node-telegram-bot-api');
+var token='Aqui va tu clave';
 var bot= new TelegramBot(
     token,{
  polling:true   
@@ -8,7 +8,7 @@ bot.getMe().then(function (me) {
     console.log('Hola mi nombre es:'+ me.username);
 });
 
-bot.onText(/^\/ yo soy(.+)$/,function (msg,match) {
+bot.onText(/^\/yo soy(.+)$/,function (msg,match) {
     var name=match[1];
     console.log(msg);
     bot.sendMessage(msg.chat.id, `¡Hola ${name} !`).then(function () {
@@ -17,7 +17,7 @@ bot.onText(/^\/ yo soy(.+)$/,function (msg,match) {
 
 });
 
-bot.onText(/^\/ foto (.+)$/,function (msg,match) {
+bot.onText(/^\/foto (.+)$/,function (msg,match) {
     var obtener=msg.chat.id;
     var photo='assets/images/perro.jpg';
      bot.sendPhoto(obtener,photo);
@@ -32,7 +32,7 @@ bot.onText(/^\/ubicacion (.+)$/,function (msg,match) {
         console.log(`Enviando ${-98.7333,20.2833}`);
     });
 
-bot.onText(/^\/ audio (.+)$/,function (msg,match) {
+bot.onText(/^\/audio (.+)$/,function (msg,match) {
     var obtener=msg.chat.id;
     var audio='assets/audio/chaleco.mp3';
      bot.sendAudio(obtener,audio);
